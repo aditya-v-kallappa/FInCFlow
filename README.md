8adf4ad8ca760c42ee1064bebb6f54145f1f25e8
geevi <girishrv@gmail.com>
2022-03-18 15:01:04 +0530

    added sample working program

--- README.md
+++ README.md
@@ -2,13 +2,9 @@
 Generating Images using Normalizing Flows with Invertible Convolution
 
 
-
-
-
 ## Developing in ada
-
+- create a python3.9 venv with all packages in requirments.txt installed in ~/venv/fastflow
 - source env.sh
-- create a python3.9 venv with all packages in requirments.txt installed
 
 
 
--- cuda/cinc_cuda/jit.py
+++ cuda/cinc_cuda/jit.py
@@ -1,4 +1,26 @@
+from numpy import dtype
 from torch.utils.cpp_extension import load
+import torch
+
 cinc_cuda = load(
     'cinc_cuda', ['cinc_cuda.cpp', 'cinc_cuda_kernel.cu'], verbose=True)
-help(cinc_cuda)
+# help(cinc_cuda)
+
+
+input = torch.tensor([[1,2,3,4],
+                      [5,6,7,8],
+                      [9,10,11,12],
+                      [13,14,15,16]
+                      ], dtype=torch.float).cuda()
+
+
+kernel = torch.tensor([[1,0],
+                       [0,1]], dtype=torch.float).cuda()
+
+
+
+output = torch.zeros((4,4), dtype=torch.float).cuda()
+
+cinc_cuda.inverse(input, kernel, output)
+
+print(output)
