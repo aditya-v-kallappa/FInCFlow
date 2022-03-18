c4619be78b57f055b6d973d880993b62d921b1f5
Girish Varma <girishvarma@outlook.com>
2022-03-18 10:03:55 +0200

    added code in comments

--- cuda/cinc_cuda/cinc_cuda_kernel.cu
+++ cuda/cinc_cuda/cinc_cuda_kernel.cu
@@ -11,19 +11,26 @@ template <typename scalar_t>
 __global__ void cinc_cuda_inverse_kernel(
     const torch::PackedTensorAccessor<scalar_t,2,torch::RestrictPtrTraits,size_t> input,
     const torch::PackedTensorAccessor<scalar_t,2,torch::RestrictPtrTraits,size_t> kernel,
-    torch::PackedTensorAccessor<scalar_t,2,torch::RestrictPtrTraits,size_t> output/*, int diag*/) {
-  //batch index
-  const int n = blockIdx.y;
-  // column index
-  const int c = blockIdx.x * blockDim.x + threadIdx.x;
-
-  // TODO: Compute inverse in a block here
-  // Each thread will compute one entry of the output
-  // We need to compute i, j based on the diagonal and c (uid for the thread)
+    torch::PackedTensorAccessor<scalar_t,2,torch::RestrictPtrTraits,size_t> output/*, int d*/) {
   
+  // thread id
+  const int tid = blockIdx.x * blockDim.x + threadIdx.x;
+
+  // Compute the index from the dth diagonal assigned to the thread
+  // int i, j;
+  // if (d < n/2) {
+  //   i = 2*d - 1 - tid;
+  //   j = tid;
+  // } else {
+  //   j = 2*d - 1 - tid;
+  //   i = tid;
+  // }
+
+  // compute entry of the output in the diagonal d assigned to this thread
+  // output[i][j] = input[i][j]
   // for (int a = k; a >= 0; a--) {
   //    for (int b = k; b >= 0; b--) {
-  //        output[i][j] = input[i][j] - kernel*output[i-a][j-b]
+  //         output[i][j] -= kernel[a][b]*output[i-a][j-b];
   //    }
   // }
 }
@@ -35,20 +42,24 @@ std::vector<torch::Tensor> cinc_cuda_inverse(
     torch::Tensor kernel,
     torch::Tensor output) {
 
-  const auto batch_size = input.size(0);
-  const auto state_size = input.size(1);
+  // assuming batch size 1 for initial implementation
+  // all the tensors above are 2D only
 
-  // for(int i = 1; i <= n; i++) {
-  // all elements of the ith diagonal must be computed in parallel
+  const auto n = input.size(0); // assuming height and width to be the same
+  const auto k = kernel.size(0); // assuming square kernel
 
-  const int threads = 1024;
-  const dim3 blocks((state_size + threads - 1) / threads, batch_size);
+  // for (int d = 1; d <= 2n - 1; d++) { // Iterating over diagonal index
+
+
+  // all elements of the dth diagonal computed in parallel
+  const int threads = 1024; // this is the recommended value for # of threads
+  const int blocks = (2*d - 1 + threads - 1) / threads; // use multiple blocks if 2d-1 > threads
 
   AT_DISPATCH_FLOATING_TYPES(input.type(), "cinc_inverse_cuda", ([&] {
     cinc_cuda_inverse_kernel<scalar_t><<<blocks, threads>>>(
         input.packed_accessor<scalar_t,2,torch::RestrictPtrTraits,size_t>(),
         kernel.packed_accessor<scalar_t,2,torch::RestrictPtrTraits,size_t>(),
-        output.packed_accessor<scalar_t,2,torch::RestrictPtrTraits,size_t>()/*, i*/);
+        output.packed_accessor<scalar_t,2,torch::RestrictPtrTraits,size_t>()/*, d*/);
   }));
 
   // synchronize all threads
